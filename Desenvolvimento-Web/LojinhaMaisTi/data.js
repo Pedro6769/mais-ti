445e3edc473f4ce3b83e6ad125d0c525d43392c3
Pedro6769 <cogitoergosum11211@gmail.com>
2026-02-06 08:43:56 -0300

    Update data.js

--- Desenvolvimento-Web/LojinhaMaisTi/data.js
+++ Desenvolvimento-Web/LojinhaMaisTi/data.js
@@ -1,1 +1,18 @@
+const Database = require('better-sqlite3');
 
+class ConexaoBanco {
+    constructor() {
+    
+   
+      this._criarTabela();
+    }
+ 
+    _criarTabela() {
+
+        const sql = 
+       
+        this.db.prepare(sql).run();
+    }
+}
+
+module.exports = new ConexaoBanco().db;
